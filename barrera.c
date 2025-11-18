5fea6707fdcd117b6c20dd0720bfd5b47c13c7c7
Javier <javier69882@gmail.com>
2025-11-18 14:27:51 -0300

    Parte1 barrera.c y .h

---
+++ barrera.c
@@ -0,0 +1,57 @@
+#include "barrera.h"
+#include <stdio.h>
+
+int barrera_init(barrera_t *b, int N){
+    if(N<=0){
+        fprintf(stderr, "Error: N debe ser mayor que 0\n");
+        return -1;
+    }
+    //incializo los campos del struct
+    b->N=N;
+    b->count=0;
+    b->etapa=0;
+    b->etapa=0;
+
+    int err;
+
+    err=pthread_mutex_init(&b->mutex, NULL);
+    if(err!=0){
+        return err;;
+    }
+    err=pthread_cond_init(&b->cond, NULL);
+    if(err!=0){
+        pthread_mutex_destroy(&b->mutex);
+    }
+    return 0;
+}
+
+int barrera_destroy(barrera_t *b){
+    int err1 = pthread_mutex_destroy(&b->mutex);
+    int err2 = pthread_cond_destroy(&b->cond);
+    
+    if(err1 != 0) return err1;
+    else return err2;
+}
+
+void barrera_wait(barrera_t *b){
+    pthread_mutex_lock(&b->mutex);
+    //etapa actual
+    int mi_etapa = b->etapa;
+
+    //esta hebra llega a la barrrera
+    b->count++;
+
+    if(b->count==b->N){
+        //ultima hebra en llegar, avanza de etapa, resetea count y despierta a todas
+        b->etapa++;
+        b->count=0;
+        pthread_cond_broadcast(&b->cond);
+    }
+    else{
+        //no es la ultima hebra, espera a que lleguen las demas
+        while(mi_etapa == b->etapa){
+            pthread_cond_wait(&b->cond, &b->mutex);
+        }
+    }
+    pthread_mutex_unlock(&b->mutex);
+}
---
+++ barrera.h
@@ -0,0 +1,29 @@
+
+ifndef BARRERA_H
+#define BARRERA_H
+#include <pthread.h>
+
+// barrera que se puede reutiliza para N hebras
+//Implemntacion usando mutex y variables de condicion, implementado como monitor
+typedef struct{
+    int count; //hebras que han llegado a la barrera acutal
+    int N; //numero de hebras que deben llegar a la barrera
+    int etapa; //etapa actual de la barrera
+    pthread_mutex_t mutex; //para la exclusion mutua
+    pthread_cond_t cond; //para la variable de condicion
+} barrera_t;
+
+
+//inicializa la barrera para sincronizar N hebras.
+//devuelve 0 si todo va bien.
+int barrera_init(barrera_t *b, int N);
+
+
+//destruye la barrera
+int barrera_destroy(barrera_t *b);
+
+
+//bloquea a la hebra hazta que lleguen N hebras a la barrera
+void barrera_wait(barrera_t *b);
+
+#endif // BARRERA_H
